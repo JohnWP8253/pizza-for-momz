b2699d279d033b74b2c82de8d2b1315e103a347e
John Pendergrass <john.pendergrass@me.com>
2023-11-09 06:40:55 -0600

    added homepage component

---
+++ src/components/Homepage/Home-component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Welcome to Pizza for Momz!</h1>
+            <p>Order your favorite pizza and we'll deliver it to your doorstep.</p>
+        </div>
+    );
+}
